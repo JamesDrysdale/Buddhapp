82f93cde185d1214018783a0438b2478ea074936
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-05-30 20:18:54 +0100

    Skeleton app with Vue3, bulma and font awesome

---
+++ src/components/HeaderBarLinks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a :href="github" target="_blank" rel="noopener noreferrer">
+            <!-- <fa :icon="[ 'fab', 'github' ]" /> -->
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a :href="twitter" target="_blank" rel="noopener noreferrer">
+            <!-- <fa :icon="[ 'fab', 'twitter' ]" /> -->
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+          <!-- <fa icon="coffee" /> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      github: 'https://github.com/johnpapa/vue-getting-started',
+      twitter: 'https://twitter.com/john_papa',
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Import Bulma CSS
+import "../node_modules/bulma/css/bulma.css";
+
+// Imports for Font Awesome
+import { dom } from '@fortawesome/fontawesome-svg-core'
+dom.watch() // This will kick of the initial replacement of i to svg tags and configure a MutationObserver
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, faTwitter, faGithub);
+
+createApp(App)
+.use(router)
+.component('fa', FontAwesomeIcon)
+.mount('#app')
